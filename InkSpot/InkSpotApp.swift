f6d35396ec79cafed9e05f9f2e7032abec7197aa
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-09 13:05:40 +0200

    Initial Commit

---
+++ InkSpot/InkSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSpotApp.swift
+//  InkSpot
+//
+//  Created by Eric on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InkSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
