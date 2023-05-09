135c61b2727b806a7d9cfbe72ba61b1569b515dd
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-09 13:17:38 +0200

    - Instantiate tatooVM

--- InkSpot/InkSpotApp.swift
+++ InkSpot/InkSpotApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct InkSpotApp: App {
+    @StateObject var tatooListVM = TatooListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             TatooListView()
+                .environmentObject(tatooListVM)
         }
     }
 }
